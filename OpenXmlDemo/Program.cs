aa692da021bd90d9e2ed767357ad6f8e8a7607e5
Muhammad Azeez <muhammad-azeez@outlook.com>
2017-11-14 14:52:17 +0300

    Setup demo

--- OpenXmlDemo/Program.cs
+++ OpenXmlDemo/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.IO;
 
 namespace OpenXmlDemo
 {
@@ -6,7 +10,51 @@ namespace OpenXmlDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var stream = new MemoryStream())
+            {
+                var document = SpreadsheetDocument.Create(stream: stream,
+                                                          type: SpreadsheetDocumentType.Workbook,
+                                                          autoSave: true);
+
+                var workbookPart = document.AddWorkbookPart();
+                var workbook = workbookPart.Workbook = new Workbook();
+                var sheetsElement = new Sheets();
+                workbook.AppendChild(sheetsElement);
+
+                InsertSheet(document, workbookPart, sheetsElement, "Sheet A", 1);
+
+                // Save the workbook and document
+                workbook.Save();
+                document.Save();
+
+                // document.Close();
+
+                // Returns an empty array if the document isn't closed
+                var bytes = stream.ToArray(); 
+                File.WriteAllBytes(@"D:\sheet.xlsx", bytes);
+            }
+        }
+
+        private static Sheet InsertSheet(SpreadsheetDocument doc,
+                                      WorkbookPart workbookpart,
+                                      Sheets sheetsElement,
+                                      string name,
+                                      uint id)
+        {
+            WorksheetPart newWorksheetPart = workbookpart.AddNewPart<WorksheetPart>();
+            newWorksheetPart.Worksheet = new Worksheet(new SheetData());
+
+            string relationshipId = doc.WorkbookPart.GetIdOfPart(newWorksheetPart);
+
+            // Give the new worksheet a name.
+            if (string.IsNullOrEmpty(name))
+                name = "Sheet" + id;
+
+            // Append the new worksheet and associate it with the workbook.
+            Sheet sheet = new Sheet() { Id = relationshipId, SheetId = id, Name = name };
+            sheetsElement.AppendChild(sheet);
+
+            return sheet;
         }
     }
 }
